18a2b483d59d9550ff7c1f8d9320f7022e1c48f9
Grama Nicolae <nicolae.grama@stud.acs.upb.ro>
2019-04-26 00:08:26 +0300

    Solved a task1 problem

--- task1.m
+++ task1.m
@@ -1,5 +1,8 @@
 function A_k = task1(image, k)
   A = double(imread(image));
-  [U,S,V] = svds(A, k);
-  A_k = U*S*V';
+  [U,S,V] = svd(A);
+  Uk = U(:,1:k);
+  Sk = S(1:k, 1:k);
+  Vk = V'(1:k, :);
+  A_k = Uk*Sk*Vk;
 end
